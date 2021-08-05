cac86d76faaf21b0b8b1fc80bcade2b466154c11
Kian-Meng, Ang <kianmeng@cpan.org>
2021-08-05 23:50:17 +0800

    docs: misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -2,10 +2,11 @@ defmodule SpiderMan.MixProject do
   use Mix.Project
   alias SpiderMan.{Component, Requester, Pipeline, Storage}
 
+  @source_url "https://github.com/feng19/spider_man"
+
   def project do
     [
       app: :spider_man,
-      description: "SpiderMan,a fast high-level web crawling & scraping framework for Elixir",
       version: "0.3.4",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -40,16 +41,21 @@ defmodule SpiderMan.MixProject do
       {:hackney, "~> 1.7", optional: true},
       {:telemetry_metrics, "~> 0.6", optional: true},
       {:logger_file_backend, "~> 0.0.11", optional: true},
-      {:ex_doc, "~> 0.22", only: [:docs, :dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:docs, :dev], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp docs do
     [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
       main: "readme",
+      source_url: @source_url,
+      formatters: ["html"],
       formatter_opts: [gfm: true],
-      extras: ["README.md", "LICENSE"],
       nest_modules_by_prefix: [Component, Requester, Pipeline, Storage]
     ]
   end
@@ -71,10 +77,11 @@ defmodule SpiderMan.MixProject do
   defp package do
     [
       name: "spider_man",
-      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      description: "SpiderMan,a fast high-level web crawling & scraping framework for Elixir",
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
       maintainers: ["feng19"],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/feng19/spider_man"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
