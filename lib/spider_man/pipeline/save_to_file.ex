236599d078782e8f4cce794f3811911a2be5e1bd
feng19 <feng_19@foxmail.com>
2021-08-24 15:20:35 +0800

    fix: SaveToFile save path to env.url

--- lib/spider_man/pipeline/save_to_file.ex
+++ lib/spider_man/pipeline/save_to_file.ex
@@ -40,7 +40,7 @@ defmodule SpiderMan.Pipeline.SaveToFile do
         if flag == :save2file_and_skip do
           :skiped
         else
-          %{data | flag: {flag, path}}
+          %{data | env: %{env | url: path}}
         end
 
       _ ->
